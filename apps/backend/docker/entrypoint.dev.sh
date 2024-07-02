519ad902de96d73c15db4107ff6e614dab93184c
Mikołaj Gawron <gawrmiko@gmail.com>
2024-07-02 21:08:17 +0200

    feat: initial commit - setting up the environment

---
+++ apps/backend/docker/entrypoint.dev.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait for PostgreSQL to be ready
+/usr/local/bin/wait-for-it.sh moprod-postgres:5432 --timeout=60 --strict -- echo "PostgreSQL is up - executing command"
+
+# Check if migrations script is present and run migrations if it is
+if npm run | grep -q "migration:run"; then
+  echo "Running database migrations"
+  npm run migration:run
+else
+  echo "No migration script found, skipping migrations"
+fi
+
+# Start Nest.js application in development mode
+npm run start:dev
