96c1312e093e9146afc3d77cd759e79b9cfd22cb
mikuull <mmikulski643@gmail.com>
2024-07-07 21:46:06 +0200

    feat: add ThemeSwitcher component

---
+++ apps/frontend/components/ThemeSwitcher.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BiMoon, BiSun } from 'react-icons/bi';
+
+import { useThemeContext } from '@/providers/ThemeProvider';
+
+export const ThemeSwitcher = () => {
+ const { theme, changeTheme } = useThemeContext();
+
+	const isDarkMode = theme === 'dark';
+
+	return (
+		<button
+			type="button"
+			onClick={() => changeTheme(isDarkMode ? 'light' : 'dark')}
+			aria-label={`${isDarkMode ? 'Wyłącz' : 'Włącz'} ciemny motyw`}
+			className="mx-auto mt-10 block text-2xl hover:text-primary sm:ml-8 sm:mt-0"
+		>
+			{theme === 'dark' ? <BiSun /> : <BiMoon />}
+		</button>
+	);
+}
