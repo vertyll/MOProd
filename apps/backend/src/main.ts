fe5eb9d8917b8d1e317b85f7092c28e919546892
Velo ⚡ <velo@wmu-hub.pl>
2024-07-19 09:31:43 +0200

    feat: create backend app

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,7 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "@/modules/app.module";
+
+(async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+})();
